6e91af92dfd682a81133270334292be7a68c71ed
Todor G Pavlov <pavlov_todor@yahoo.co.uk>
2024-01-23 22:26:23 +0000

    Little fix

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 
 rock = 'Rock'
 paper = 'Paper'
@@ -6,23 +6,23 @@ scissors = 'Scissors'
 
 player_move = input('Please select one of the following -[r]ock, pa[p]er, scissor[s]- :')
 
-if player_move.lower() == 'r' or 'rock':
+if player_move.lower() == 'r' or player_move.lower() == 'rock':
     player_move = rock
-elif player_move.lower() == 'p':
+elif player_move.lower() == 'p' or player_move.lower() == 'paper':
     player_move = paper
-elif player_move.lower() == 's':
+elif player_move.lower() == 's' or player_move.lower() == 'scissors':
     player_move = scissors
 else:
     raise SystemExit('Invalid Input. Please try again ...')
-computer_move = random.choice((rock, paper, scissors))
+computer_move = choice((rock, paper, scissors))
 
-print(f'Computer have chosen: {computer_move}')
+print(f'{player_move.upper()} vs {computer_move.upper()}')
 
 if (player_move == rock and computer_move == scissors) or \
         (player_move == paper and computer_move == rock) or \
         (player_move == scissors and computer_move == paper):
-    print('You Win!')
+    print('You Win')
 elif player_move == computer_move:
-    print('It\'s a Draw!')
+    print('Draw')
 else:
-    print('You Lose!')
+    print('You Lose')
