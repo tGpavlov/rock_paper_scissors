177fb7069b79c28bac26a7712f562a77b0e53efc
Todor G Pavlov <pavlov_todor@yahoo.co.uk>
2024-01-26 16:35:01 +0000

    ver. 1.1
    - Upgrade UI
    - Added some colour
    - Added score board

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,10 +1,20 @@
 from random import choice
+from colorama import Fore, init
 
-rock = 'Rock'
-paper = 'Paper'
-scissors = 'Scissors'
+rock = Fore.BLUE + 'Rock'
+paper = Fore.LIGHTYELLOW_EX + 'Paper'
+scissors = Fore.LIGHTWHITE_EX + 'Scissors'
+wins = 0
+draws = 0
+loses = 0
+init(autoreset=True)
 
-player_move = input('Please select one of the following -[r]ock, pa[p]er, scissor[s]- :')
+print()
+
+player_move = input(Fore.LIGHTWHITE_EX + 'Please select one of the following: '
+                    + Fore.BLUE + '-[r]ock' + ', ' + Fore.LIGHTYELLOW_EX + 'pa[p]er'
+                    + ', ' + Fore.LIGHTWHITE_EX + 'scissor[s]- :')
+print()
 
 if player_move.lower() == 'r' or player_move.lower() == 'rock':
     player_move = rock
@@ -16,13 +26,27 @@ else:
     raise SystemExit('Invalid Input. Please try again ...')
 computer_move = choice((rock, paper, scissors))
 
-print(f'{player_move.upper()} vs {computer_move.upper()}')
+print('------------------')
+print(Fore.WHITE + 'Player: ' + f'{player_move}')
+print('--------' + Fore.RED + 'vs' + Fore.RESET + '--------')
+print(Fore.WHITE + 'Computer: ' + f'{computer_move}')
+print('------------------')
 
-if (player_move == rock and computer_move == scissors) or \
+if player_move == computer_move:
+    draws += 1
+    print(Fore.YELLOW + ' > It\'s a Draw! <')
+    is_draw = True
+elif (player_move == rock and computer_move == scissors) or \
         (player_move == paper and computer_move == rock) or \
         (player_move == scissors and computer_move == paper):
-    print('You Win')
-elif player_move == computer_move:
-    print('Draw')
+    wins += 1
+    print(Fore.GREEN + '  > You Win! <')
+    is_win = True
 else:
-    print('You Lose')
+    print(Fore.RED + '  > You Lose! <')
+    loses += 1
+
+total_games = wins + loses + draws
+print()
+print(Fore.WHITE + f'Total games: {total_games}', Fore.GREEN + f'Wins: {wins}', Fore.YELLOW +
+      f'Draws: {draws}', Fore.RED + f'Loses: {loses}', sep='\n')
